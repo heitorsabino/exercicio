4e43c8845f2a68c54c03088f53d5ad60924815cb
heitor martins <164927390+heitorsabino@users.noreply.github.com>
2024-05-06 10:22:59 -0300

    Update tempoViagem.py

--- api/tempoViagem.py
+++ api/tempoViagem.py
@@ -1,4 +1,4 @@
-
+nome = input("Digite seu nome: ")
 
 distancia = float(input("Digite a distância da viagem (em km): "))
 
@@ -6,4 +6,4 @@ velocidade = float(input("Digite a velocidade média da viagem (em km/h): "))
 
 tempo = distancia / velocidade
 
-print(f"O tempo de viagem será de aproximadamente {tempo:.2f} horas.")
+print(f"Olá {nome}, o tempo de viagem será de aproximadamente {tempo:.2f} horas.")
